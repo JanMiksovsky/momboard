e7e79364f373a1cd66d3684e1b6f55d6805439a8
Jan Miksovsky <jan@miksovsky.com>
2024-11-16 17:11:56 -0800

    Build secrets.js from environment variables

---
+++ build.sh
@@ -0,0 +1,7 @@
+# This "build" script only writes some environment variables to
+# `public/secrets.js` so the JavaScript files can read them.
+
+echo "/* App secrets */" > public/secrets.js
+echo "export const apiKey = \"$JSON_STORAGE_API_KEY\";" >> public/secrets.js
+echo "export const itemId = \"$JSON_STORAGE_ITEM_ID\";" >> public/secrets.js
+echo "export const userId = \"$JSON_STORAGE_USER_ID\";" >> public/secrets.js
